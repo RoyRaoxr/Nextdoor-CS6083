f9ad23a02bcd905fb5e0f7d1823a3b5639096bbc
Megathrone <yizhouwang418@gmail.com>
2019-12-03 22:14:40 -0500

    add main page

---
+++ nextdoor/src/main/java/edu/nyu/cs6083/nextdoor/controller/LoginController.java
@@ -0,0 +1,57 @@
+package edu.nyu.cs6083.nextdoor.controller;
+
+
+import edu.nyu.cs6083.nextdoor.bean.User;
+import edu.nyu.cs6083.nextdoor.dao.UserDao;
+import javax.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class LoginController {
+
+    private final Logger logger = LoggerFactory.getLogger(LoginController.class.getSimpleName());
+
+    @Autowired
+    UserDao userDao;
+
+
+    @GetMapping("/signup")
+    public String signupPage(Model m) {
+        m.addAttribute("user", new User());
+        return "signup";
+    }
+
+    @RequestMapping("/login")
+    public String userLogin(@ModelAttribute User user, Model m,
+        HttpServletRequest request) {
+        User currUser = userDao.findByUsernameAndPassword(user.getUsername(), user.getPassword());
+        if (currUser == null) {
+            m.addAttribute("err", true);
+            return "index";
+        }
+        request.getSession(true).setAttribute("useradmin", currUser);
+        return "redirect:/main";
+    }
+
+    @PostMapping("/signup")
+    public String userSignup(@ModelAttribute User user, Model m) {
+        boolean exists = userDao.findByUsername(user.getUsername()) != null;
+        if (exists) {
+            m.addAttribute("userExists", true);
+            return "signup";
+        }
+
+        userDao.save(user);
+
+        return "redirect:/";
+    }
+}
--- nextdoor/src/main/java/edu/nyu/cs6083/nextdoor/controller/MainController.java
+++ nextdoor/src/main/java/edu/nyu/cs6083/nextdoor/controller/MainController.java
@@ -3,20 +3,14 @@ package edu.nyu.cs6083.nextdoor.controller;
 
 import edu.nyu.cs6083.nextdoor.bean.User;
 import edu.nyu.cs6083.nextdoor.dao.UserDao;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 
-    private final Logger logger = LoggerFactory.getLogger(MainController.class.getSimpleName());
 
     @Autowired
     UserDao userDao;
@@ -27,33 +21,9 @@ public class MainController {
         return "index";
     }
 
-    @GetMapping("/signup")
-    public String signupPage(Model m) {
-        m.addAttribute("user", new User());
-        return "signup";
-    }
 
-    @RequestMapping("/login")
-    public String userLogin(@ModelAttribute User user, Model m) {
-        User currUser = userDao.findByUsernameAndPassword(user.getUsername(), user.getPassword());
-        if (currUser == null) {
-            m.addAttribute("err", true);
-            return "index";
-        }
-
-        return "login200";
-    }
-
-    @PostMapping("/signup")
-    public String userSignup(@ModelAttribute User user, Model m) {
-        boolean exists = userDao.findByUsername(user.getUsername()) != null;
-        if (exists) {
-            m.addAttribute("userExists", true);
-            return "signup";
-        }
-
-        userDao.save(user);
-
-        return "redirect:/";
+    @GetMapping("main")
+    public String mainPage() {
+        return "main/main";
     }
 }

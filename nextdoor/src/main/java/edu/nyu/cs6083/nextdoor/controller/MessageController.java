d9b7e6d95f7c60a5247b34d641d19379a613abcf
Megathrone <yizhouwang418@gmail.com>
2019-12-13 01:41:34 -0500

    fix

--- nextdoor/src/main/java/edu/nyu/cs6083/nextdoor/controller/MessageController.java
+++ nextdoor/src/main/java/edu/nyu/cs6083/nextdoor/controller/MessageController.java
@@ -15,6 +15,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MessageController {
@@ -52,8 +53,8 @@ public class MessageController {
     }
 
     @PostMapping("replysmsg")
-    public String replyMsg() {
-        return "login200";
+    public @ResponseBody String replyMsg() {
+        return "NMSL";
     }
 
     @GetMapping("message")

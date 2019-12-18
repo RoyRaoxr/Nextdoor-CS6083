544b081d78eeee3f4430106a3d92c9805d355dd4
Megathrone <yizhouwang418@gmail.com>
2019-12-18 16:52:26 -0500

    xss filter

---
+++ nextdoor/src/main/java/edu/nyu/cs6083/nextdoor/config/XssAndSqlHttpServletRequestWrapper.java
@@ -0,0 +1,39 @@
+package edu.nyu.cs6083.nextdoor.config;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.text.StringEscapeUtils;
+
+public class XssAndSqlHttpServletRequestWrapper extends HttpServletRequestWrapper {
+
+    private HttpServletRequest request;
+
+    public XssAndSqlHttpServletRequestWrapper(HttpServletRequest request) {
+        super(request);
+        this.request = request;
+    }
+
+    @Override
+    public String getParameter(String name) {
+        String value = request.getParameter(name);
+        if (!StringUtils.isEmpty(value)) {
+            value = StringEscapeUtils.escapeHtml4(value);
+        }
+        return value;
+    }
+
+    @Override
+    public String[] getParameterValues(String name) {
+        String[] parameterValues = super.getParameterValues(name);
+        if (parameterValues == null) {
+            return null;
+        }
+        for (int i = 0; i < parameterValues.length; i++) {
+            String value = parameterValues[i];
+            parameterValues[i] = StringEscapeUtils.escapeHtml4(value);
+        }
+        return parameterValues;
+    }
+}
+
---
+++ nextdoor/src/main/java/edu/nyu/cs6083/nextdoor/config/XssFilter.java
@@ -0,0 +1,42 @@
+package edu.nyu.cs6083.nextdoor.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.stereotype.Component;
+
+@WebFilter
+@Component
+public class XssFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+        throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        XssAndSqlHttpServletRequestWrapper xssRequestWrapper = new XssAndSqlHttpServletRequestWrapper(
+            req);
+        chain.doFilter(xssRequestWrapper, response);
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+
+}
+
